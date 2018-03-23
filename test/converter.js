0b1e59bba73e8911ac27c341e2d6fbe7a3ac61c4
DarisCalinor <candogan.tuzcu@gmail.com>
2018-03-23 22:58:22 +1100

    Add test for coverter functionality

--- test/converter.js
+++ test/converter.js
@@ -1,14 +1,12 @@
-/* eslint-disable */
+const { expect } = require("chai");
+const { hexToRgb, rgbToHex } = require("../app/converter");
 
-const {expect} = require("chai");
-const converter = require("../app/coverter");
-
-describe("Color Code Converter", function () {
-  describe("RGB to Hex conversion", function() {
-    it("converts the basic colors", function() {
-      const redHex = converter.rgbToHex(255, 0, 0);
-      const greenHex = converter.rgbToHex(0, 255, 0);
-      const blueHex = converter.rgbToHex(0, 0, 255);
+describe("Color Code Converter", () => {
+  describe("RGB to Hex conversion", () => {
+    it("converts the basic colors", () => {
+      const redHex = rgbToHex(255, 0, 0);
+      const greenHex = rgbToHex(0, 255, 0);
+      const blueHex = rgbToHex(0, 0, 255);
 
       expect(redHex).to.equal("ff0000");
       expect(greenHex).to.equal("00ff00");
@@ -16,11 +14,11 @@ describe("Color Code Converter", function () {
     });
   });
 
-  describe("Hex to RGB conversion", function() {
-    it("converts the basic colors", function(){
-      const red = converter.hexToRgb("ff0000");
-      const green = converter.hexToRgb("00ff00");
-      const blue = converter.hexToRgb("0000ff");
+  describe("Hex to RGB conversion", () => {
+    it("converts the basic colors", () => {
+      const red = hexToRgb("ff0000");
+      const green = hexToRgb("00ff00");
+      const blue = hexToRgb("0000ff");
 
       expect(red).to.deep.equal([255, 0, 0]);
       expect(green).to.deep.equal([0, 255, 0]);
