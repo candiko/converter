4698c7ba3178cfcbe543b10b863d16b41c7ce722
DarisCalinor <candogan.tuzcu@gmail.com>
2018-03-23 23:34:23 +1100

    Implement Color Convert API

---
+++ app/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const { rgbToHex, hexToRgb } = require("./converter");
+
+app.get("/rgbToHex", (req, res) => {
+  const red = parseInt(req.query.red, 10);
+  const green = parseInt(req.query.green, 10);
+  const blue = parseInt(req.query.blue, 10);
+
+  const hex = rgbToHex(red, green, blue);
+
+  res.send(hex);
+});
+
+app.get("/hexToRgb", (req, res) => {
+  const { hex } = req.query;
+
+  const rgb = hexToRgb(hex);
+
+  res.send(JSON.stringify(rgb));
+});
+
+app.listen(3000);
